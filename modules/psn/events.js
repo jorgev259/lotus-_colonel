1de2c0635114eb716774ff5da40591c087f9e06e
Jorge Vargas <jorgev259@outlook.es>
2019-06-13 16:23:16 -0600

    test

--- modules/psn/events.js
+++ modules/psn/events.js
@@ -14,7 +14,7 @@ module.exports = {
       let members = await guild.members.fetch()
       await guild.roles.fetch()
       let count = 0
-      console.log(guild.roles.map(function (r) { return { name: r.name, pos: r.position } }))
+      console.log(guild.roles.filter(r => r.position < 39).map(function (r) { return { name: r.name, pos: r.position } }))
       // let promises = guild.roles.filter(r => r.position > 0 && r.position < 68).map(r => /* r.delete() */ console.log(r.name))
       // await Promise.all(promises)
       console.log('done')
