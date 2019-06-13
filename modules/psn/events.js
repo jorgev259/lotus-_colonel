35257aac9b91f22f63c4a5683dbbd38fd2602307
Jorge Vargas <jorgev259@outlook.es>
2019-06-13 16:38:55 -0600

    test

--- modules/psn/events.js
+++ modules/psn/events.js
@@ -18,7 +18,7 @@ module.exports = {
       members.forEach(member => {
         let rolePos = guild.roles.find(r => r.name === '// PSN Colors')
         member.guild.roles.create({ data: { name: member.user.username, position: rolePos.position - 1 } }).then(role => {
-          db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.user.username, member.user.username, role.id)
+          db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.id, member.user.username, role.id)
 
           count++
           console.log(`${count}/${members.size}`)
@@ -30,11 +30,10 @@ module.exports = {
       await member.guild.roles.fetch()
       let rolePos = member.guild.roles.find(r => r.name === '// PSN Colors')
       member.guild.roles.create({ data: { name: member.user.username, position: rolePos.position - 1 } }).then(role => {
-        db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.user.username, member.user.username, role.id)
+        db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.id, member.user.username, role.id)
+        member.roles.add(role)
       })
       member.guild.systemChannel.send(`Welcome to The Disflavored, ${member}`)
-
-      console.log(member)
     }
   }
 }
