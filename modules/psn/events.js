e7de9a36516e2d980250924d5eb4653e1cbcf7af
Jorge Vargas <jorgev259@outlook.es>
2019-06-13 16:48:17 -0600

    test

--- modules/psn/events.js
+++ modules/psn/events.js
@@ -13,17 +13,36 @@ module.exports = {
       let guild = client.guilds.first()
       let members = await guild.members.fetch()
       await guild.roles.fetch()
-      let count = 0
 
       members.forEach(member => {
-        let rolePos = guild.roles.find(r => r.name === '// PSN Colors')
-        member.guild.roles.create({ data: { name: member.user.username, position: rolePos.position - 1 } }).then(role => {
-          db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.id, member.user.username, role.id)
-
-          count++
-          console.log(`${count}/${members.size}`)
-          member.roles.add(role)
-        })
+        try {
+          let entry = db.prepare('SELECT * FROM psn WHERE user=?').get(member.id)
+          if (entry) {
+            if (guild.roles.some(r => r.name === entry.psn)) member.roles.add(guild.roles.find(r => r.name === entry.psn))
+            else {
+              let rolePos = guild.roles.find(r => r.name === '// PSN Colors')
+              member.guild.roles.create({ data: { name: member.user.username, position: rolePos.position - 1 } }).then(role => {
+                db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.id, member.user.username, role.id)
+                member.roles.add(role)
+              })
+            }
+          } else {
+            if (guild.roles.some(r => r.name === entry.psn)) {
+              let role = guild.roles.find(r => r.name === entry.psn)
+              db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.id, member.user.username, role.id)
+              member.roles.add(role)
+            } else {
+              let rolePos = guild.roles.find(r => r.name === '// PSN Colors')
+              member.guild.roles.create({ data: { name: member.user.username, position: rolePos.position - 1 } }).then(role => {
+                db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.id, member.user.username, role.id)
+                member.roles.add(role)
+              })
+            }
+          }
+        } catch (err) {
+          console.log(err)
+          console.log(member)
+        }
       })
     },
     async guildMemberAdd (client, db, moduleName, member) {
