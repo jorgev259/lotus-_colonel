09cff6c51abf7dc8dc54434132ee828f35267cb3
Jorge Vargas <jorgev259@outlook.es>
2019-06-13 15:56:31 -0600

    test

--- modules/psn/events.js
+++ modules/psn/events.js
@@ -1,7 +1,7 @@
 module.exports = {
   reqs (client, db, moduleName) {
     return new Promise((resolve, reject) => {
-      db.prepare('CREATE TABLE IF NOT EXISTS psn (user TEXT, psn TEXT, PRIMARY KEY (user))').run()
+      db.prepare('CREATE TABLE IF NOT EXISTS psn (user TEXT, psn TEXT, role TEXT, PRIMARY KEY (user))').run()
       resolve()
     })
   },
@@ -9,7 +9,29 @@ module.exports = {
     default: true
   },
   events: {
+    async ready (client, db) {
+      let guild = client.guilds.first()
+      let members = await guild.members.fetch()
+      await guild.roles.fetch()
+      let count = 0
+      let rolePos = guild.roles.find(r => r.name === '// PSN colors')
+
+      members.forEach(member => {
+        member.guild.roles.create({ data: { name: member.user.username, position: rolePos.position } }).then(role => {
+          db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.user.username, member.user.username, role.id)
+
+          count++
+          console.log(`${count}/${members.size}`)
+          process.exit(1)
+        })
+      })
+    },
     async guildMemberAdd (client, db, moduleName, member) {
+      member.guild.roles.create({ data: { name: member.user.username } }).then(role => {
+        db.prepare('INSERT INTO psn (user,psn,role) VALUES (?,?,?)').run(member.user.username, member.user.username, role.id)
+      })
+      member.guild.systemChannel.send(`Welcome to The Disflavored, ${member}`)
+
       console.log(member)
     }
   }
